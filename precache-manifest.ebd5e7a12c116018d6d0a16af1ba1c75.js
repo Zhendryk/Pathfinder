6812ca1ec1bcdbbb5f1d0a889e61177d566c4351
Zhendryk <jonathan.bailey@comcast.net>
2020-06-28 21:48:05 -0400

    Updates

---
+++ precache-manifest.ebd5e7a12c116018d6d0a16af1ba1c75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61606df470f86b584ba7cfa706db8338",
+    "url": "/Pathfinder/index.html"
+  },
+  {
+    "revision": "d783ca52bf32e821e950",
+    "url": "/Pathfinder/static/css/main.8703876b.chunk.css"
+  },
+  {
+    "revision": "2d92bc9198b55702ffc1",
+    "url": "/Pathfinder/static/js/2.60c7eb7a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Pathfinder/static/js/2.60c7eb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d783ca52bf32e821e950",
+    "url": "/Pathfinder/static/js/main.aef364e5.chunk.js"
+  },
+  {
+    "revision": "8a41312bfe2329ad4cb0",
+    "url": "/Pathfinder/static/js/runtime-main.14b98dad.js"
+  },
+  {
+    "revision": "fe2f71a74cc21f2a579b6bd6181680c5",
+    "url": "/Pathfinder/static/media/goal.fe2f71a7.png"
+  }
+]);
